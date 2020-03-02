974c2b29642277ed3e978874c1aee2ad55cfc455
Pietro Bonechi <p.bonechi1@gmail.com>
2020-03-02 21:45:34 +0100

    Fix login, add style for some pages and fix signon

--- Backend/addUser.php
+++ Backend/addUser.php
@@ -1,8 +1,8 @@
 <?php
     //Dati accesso DB
-$servername = "127.0.0.1:3306";
-$username = "quintab";
-$password = "HA45@BMV";
+$servername = "localhost:3306";
+$username = "bonex"; //"quintab";
+$password = "Pb12112001";//"HA45@BMV";
 $db = "quintab_es.4";
 
 // Create connection  {"user":"bonex","pass":"123456"}
@@ -10,15 +10,18 @@ if($_GET["sesso"]=="Maschio")
 $_GET["sesso"] = 'M';
 else
 $_GET["sesso"] = 'F';
-if($_GET["patB"]=="on")
-$_GET["patB"] = 1;
+if(isset($_GET["patB"]))
+    $_GET["patB"] = 1;    
 else
-$_GET["patB"]= 0;
-if($_GET["patA"]=="on")
-$_GET["patA"] = 1;
+    $_GET["patB"]= 0;
+
+if(isset($_GET["patA"]))
+    $_GET["patA"] = 1;
 else
 $_GET["patA"]= 0;
-    	$conn = mysqli_connect($servername,$username,$password,$db) or die('Errore...');
+
+$_GET["password"] = md5($_GET["password"]);
+    	$conn = mysqli_connect($servername,$username,$password,$db) or die( mysqli_connect_error());
        $query = "INSERT INTO `utenti` (`nome`, `cognome`, `email`, `pass`, `patA`, `patB`, `user`, `nazionalita`, `sesso`) VALUES ('".$_GET["nome"]."', '".$_GET["cognome"]."', '".$_GET["email"]."', '".$_GET["password"]."', '".$_GET["patA"]."', '".$_GET["patB"]."', '".$_GET["user"]."', '".$_GET["naz"]."', '".$_GET["sesso"]."')";
        mysqli_query($conn, $query) or die( mysqli_error($conn));
        echo $query;
@@ -31,29 +34,22 @@ $_GET["patA"]= 0;
 </head>
     <body>
     <div class = 'all'
-    style='width: 450px;'>
-        <div  style='
-        background-color: green;
-        border: 1px solid;
-        border-radius: 14px;'>
+    style="text-align: center; margin-top: 10%; background-color: yellow; margin-left: 20%; margin-right: 20%;border-style: solid; border-radius: 20px;">
         Riepilogo dati:
         </div>
-    <div style='text-align: left;
-    background-color: red;
-    border: 1px solid;
-        border-radius: 14px;'>
+    <div style="text-align: center; margin-top: 10%; background-color: yellow; margin-left: 20%; margin-right: 20%;border-style: solid; border-radius: 20px;">
         
         Cognome =  <?php echo $_GET["cognome"]?><br>
         Nome =    <?php echo $_GET["nome"]?><br>
         User =  <?php echo $_GET["user"]?><br>
         Sesso =    <?php echo $_GET["sesso"]?><br>
         Nazione =  <?php echo $_GET["naz"]?><br>
-       Patente/i =  <?php if($_GET["patA"] =="on")
+       Patente/i =  <?php if($_GET["patA"] ==1)
                             echo A;
-                            if($_GET["patB"] =="on")
+                            if($_GET["patB"] ==1)
                             echo B;?><br>
         Email =     <?php echo $_GET["email"]?><br>
-        <form action='http://localhost/informatica/5B%20IA/php/Es.4'>
+        <form action='../Frontend/index.html'>
             <input class='btn btn-primary' type='submit' value='<- BACK' />
         </form>
         <label for="exampleFormControlSelect1">Registrazione avvenuta!</label>
--- Backend/searchUser.php
+++ Backend/searchUser.php
@@ -2,21 +2,20 @@
 function checkUser($user,$pass){ 
     //Dati accesso DB
 $servername = "127.0.0.1:3306";
-$username = "quintab";
-$password = "HA45@BMV";
+$username = "bonex";
+$password = "Pb12112001";
 $db = "quintab_es.4";
 
 // Create connection  {"user":"bonex","pass":"123456"}
     	$conn = mysqli_connect($servername,$username,$password,$db) or die('Errore...');
        $query = "SELECT * FROM utenti";
        $result = mysqli_query($conn, $query);
-
+       $pass = md5($pass);
 if ($result->num_rows > 0) {
     //output data of each row
     while($row = $result->fetch_assoc()) {
-      echo $row['user'];
-      echo $row['password'];
-    if($user == $row['user'] || $pass == $row['password'])
+
+    if(strcmp($user,$row['user']) == 0 && strcmp($pass,$row['pass']) == 0)
     { 
       mysqli_close ($conn);
      return true;
@@ -47,12 +46,11 @@ echo("Utente errato");
 
       if(checkUser($_GET["user"],$_GET["password"])== true)
       {
-        echo ($_GET["user"].$_GET["password"]);
         echo'<div class="reg">
         <div class="form-group">
       <label for="exampleFormControlInput1">Accesso eseguito!</label>
       </div>
-          <form method="GET" action="http://localhost/informatica/5B%20IA/php/Es.4">           
+          <form method="GET" action="../Frontend/index.html">           
                 <div class="col-auto my-1">
                   <button  class="btn btn-Danger"><- Back</button>
                 </div>
@@ -64,7 +62,7 @@ echo("Utente errato");
           <div class="form-group">
         <label for="exampleFormControlInput1">Utente non presente!</label>
         </div>
-            <form method="GET" action="http://localhost/informatica/5B%20IA/php/Es.4">           
+            <form method="GET" action="../Frontend/index.html">           
                   <div class="col-auto my-1">
                     <button  class="btn btn-Danger"><- Back</button>
                   </div>
